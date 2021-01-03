618aac047e57953e241e5d0f8990694f079d5fc9
ollie-cook <olliewatford@gmail.com>
2021-01-03 19:28:42 +0000

    tile disappears from top on both screens

--- client/scripth.js
+++ client/scripth.js
@@ -186,8 +186,6 @@ document.addEventListener("DOMContentLoaded", () => {
           if (bankTotal1 >= tileArray[tileId].number) {
             if (bank1.includes(diceArray[5])) {
               //This is what happens if the bank total is high enough and they have banked a worm.
-              pile1.push(tileArray[tileId]);
-              this.setAttribute("src", "https://i.ibb.co/Cw6ySLH/blue.png");
               returnChecker = 1;
               socket.emit("bankTile", {
                 counter: counter,
@@ -213,9 +211,6 @@ document.addEventListener("DOMContentLoaded", () => {
         if (rollCounter === bank2[bank2.length - 1].roll) {
           if (bankTotal2 >= tileArray[tileId].number) {
             if (bank2.includes(diceArray[5])) {
-              pile2.push(tileArray[tileId]);
-              this.setAttribute("src", "https://i.ibb.co/Cw6ySLH/blue.png");
-
               returnChecker = 1;
               socket.emit("bankTile", {
                 counter: counter,
@@ -252,12 +247,16 @@ document.addEventListener("DOMContentLoaded", () => {
     var counter = data.counter;
     console.log("running");
     if (counter % 2 !== 0) {
+      pile1.push(tileArray[tileId]);
+      this.setAttribute("src", "https://i.ibb.co/Cw6ySLH/blue.png");
       var piles1 = pileOne.querySelectorAll("img");
       piles1[0].setAttribute("src", pile1[pile1.length - 1].img);
       piles1[0].setAttribute("data-id", tileId);
       piles1[0].addEventListener("click", returnSteal);
       piles1[0].setAttribute("class", "tile1");
-    } else {
+    } else { 
+      pile2.push(tileArray[tileId]);
+      this.setAttribute("src", "https://i.ibb.co/Cw6ySLH/blue.png");
       var piles2 = pileTwo.querySelectorAll("img");
       piles2[0].setAttribute("src", pile2[pile2.length - 1].img);
       piles2[0].setAttribute("data-id", tileId);
@@ -353,8 +352,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     finish();
   }
-
-
 });
 
 const diceArray = [
